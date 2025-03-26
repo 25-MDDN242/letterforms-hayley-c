08f38368bd8ca2c406bb0270b9e4dc8a03bdc867
hayley-c <hj.chung24@gmail.com>
2025-03-27 11:18:59 +1300

    Moving over to draw letters
    Moved my sketched letters into draw letters. I am now trying to fit the sketch letters into the lines and boxes.

--- letters.js
+++ letters.js
@@ -7,23 +7,69 @@ const alphabet = {
   "A": {
     "size": 40,
     "offsetx": 0,
-    "offsety": 17
+    "offsety": 17,
+
+    "AnchorX": 90,
+    "AnchorY": -50,
+    "firstControlX": -20,
+    "firstControlY": -100,
+    "secondControlX": -30,
+    "secondControlY": 200,
+  
+    "stem": true,
+    "secondAnchorX": 90,
+    "secondAnchorY": -50,
+    "twoFirstControlX": 80,
+    "twoFirstControlY": -100,
+    "twoSecondControlX": 100,
+    "twoSecondControlY": 200
   },
   "B": {
     "size": 75,
     "offsetx": 0,
-    "offsety": -70
+    "offsety": -70,
+
+    "AnchorX": 0,
+    "AnchorY": 30,
+    "firstControlX": 80,
+    "firstControlY": -150,
+    "secondControlX": 100,
+    "secondControlY": 120,
+  
+    "stem": true,
+    "secondAnchorX": 0,
+    "secondAnchorY": 30,
+    "twoFirstControlX": 40,
+    "twoFirstControlY": -210,
+    "twoSecondControlX": 10,
+    "twoSecondControlY": -130
   },
   "C": {
     "size": 50,
     "offsetx": 15,
-    "offsety": 0
+    "offsety": 0,
+
+    "AnchorX": 30,
+    "AnchorY": 0,
+    "firstControlX": -60,
+    "firstControlY": -120,
+    "secondControlX": -100,
+    "secondControlY": 200,
+    
+    "stem": false,
+    "secondAnchorX": 0,
+    "secondAnchorY": 0,
+    "twoFirstControlX": 0,
+    "twoFirstControlY": 0,
+    "twoSecondControlX": 0,
+    "twoSecondControlY": 0
   },
   "D": {
     "size": 50,
     "offsetx": 15,
     "offsety": 0
   },
+
   "E": {
     "size": 50,
     "offsetx": 15,
@@ -87,7 +133,22 @@ const alphabet = {
   "Q": {
     "size": 50,
     "offsetx": 15,
-    "offsety": 0
+    "offsety": 0,
+
+    "AnchorX": 100,
+    "AnchorY": -50,
+    "firstControlX": -60,
+    "firstControlY": -30,
+    "secondControlX": -50,
+    "secondControlY": 200,
+  
+    "stem": true,
+    "secondAnchorX": 40,
+    "secondAnchorY": 0,
+    "twoFirstControlX": 20,
+    "twoFirstControlY": -20,
+    "twoSecondControlX": 40,
+    "twoSecondControlY": 200
   },
   "R": {
     "size": 50,
