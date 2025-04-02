49c467451d4e9dca87c2201bfb42f0c2acf2733d
hayley-c <hj.chung24@gmail.com>
2025-04-02 20:38:07 +1300

    Continued filling alphabet
    Add more letters to the layered bezier alphabet. Made a slight change to the stem style.

--- draw_letters.js
+++ draw_letters.js
@@ -19,34 +19,45 @@ const spacing = 10;
  * from (0,0) to (100, 200)
  */
 function drawLetter(letterData) {
-  color/stroke setup
   stroke(255);
   strokeWeight(2);
   noFill();
 
-  let firstBezierAnchorX = 0 + letterData["AnchorX"];
-  let firstBezierAnchorY = 150 + letterData["AnchorY"];
-  let firstBezierControlX = 0 + letterData["firstControlX"];
-  let firstBezierControlY = 150 + letterData["firstControlY"];
-  let secondBezierControlX = 0 + letterData["secondControlX"];
-  let secondBezierControlY = 150 + letterData["secondControlY"];
+  let bezierOneX1 = 0 + letterData["oneVar1"];
+  let bezierOneY1 = 150 + letterData["oneVar2"];
+  let bezierOneX2 = 0 + letterData["oneVar3"];
+  let bezierOneY2 = 150 + letterData["oneVar4"];
+  let bezierOneX3 = 0 + letterData["oneVar5"];
+  let bezierOneY3 = 150 + letterData["oneVar6"];
+  let bezierOneX4 = 0 + letterData["oneVar7"];
+  let bezierOneY4 = 150 + letterData["oneVar8"];
  
-  let twoFirstBezierAnchorX = 0 + letterData["secondAnchorX"];
-  let twoFirstBezierAnchorY = 150 + letterData["secondAnchorY"];
-  let twoFirstBezierControlX = 0 + letterData["twoFirstControlX"];
-  let twoFirstBezierControlY = 150 + letterData["twoFirstControlY"];
-  let twoSecondBezierControlX = 0 + letterData["twoSecondControlX"];
-  let twoSecondBezierControlY = 150 + letterData["twoSecondControlY"];
+  let bezierTwoX1 = 0 + letterData["twoVar1"];
+  let bezierTwoY1 = 150 + letterData["twoVar2"];
+  let bezierTwoX2 = 0 + letterData["twoVar3"];
+  let bezierTwoY2 = 150 + letterData["twoVar4"];
+  let bezierTwoX3 = 0 + letterData["twoVar5"];
+  let bezierTwoY3 = 150 + letterData["twoVar6"];
+  let bezierTwoX4 = 0 + letterData["twoVar7"];
+  let bezierTwoY4 = 150 + letterData["twoVar8"]
 
-  bezier(firstBezierAnchorX, firstBezierAnchorY, firstBezierControlX, firstBezierControlY, secondBezierControlX, secondBezierControlY, firstBezierAnchorX, firstBezierAnchorY);
-  bezier(firstBezierAnchorX, firstBezierAnchorY, firstBezierControlX + spacing, firstBezierControlY + spacing, secondBezierControlX + spacing, secondBezierControlY - 2 * spacing, firstBezierAnchorX, firstBezierAnchorY);
-  bezier(firstBezierAnchorX, firstBezierAnchorY, firstBezierControlX + 2 * spacing, firstBezierControlY + 2 * spacing, secondBezierControlX + 2 * spacing, secondBezierControlY - 4 * spacing, firstBezierAnchorX, firstBezierAnchorY);
+  bezier(bezierOneX1, bezierOneY1, bezierOneX2, bezierOneY2, bezierOneX3, bezierOneY3, bezierOneX4, bezierOneY4);
+  bezier(bezierOneX1, bezierOneY1, bezierOneX2 + spacing, bezierOneY2 + spacing, bezierOneX3 + spacing, bezierOneY3 - spacing, bezierOneX4, bezierOneY4);
+  bezier(bezierOneX1, bezierOneY1, bezierOneX2 + 2 * spacing, bezierOneY2 + 2 * spacing, bezierOneX3 + 2 * spacing, bezierOneY3 - 2 * spacing, bezierOneX4, bezierOneY4);
 
-  bezier(twoFirstBezierAnchorX, twoFirstBezierAnchorY, twoFirstBezierControlX, twoFirstBezierControlY, twoSecondBezierControlX, twoSecondBezierControlY, twoFirstBezierAnchorX, twoFirstBezierAnchorY);
-  bezier(twoFirstBezierAnchorX, twoFirstBezierAnchorY, twoFirstBezierControlX + spacing, twoFirstBezierControlY + spacing, twoSecondBezierControlX + spacing, twoSecondBezierControlY - 2 * spacing, twoFirstBezierAnchorX, twoFirstBezierAnchorY);
-  bezier(twoFirstBezierAnchorX, twoFirstBezierAnchorY, twoFirstBezierControlX + 2 * spacing, twoFirstBezierControlY + 2 * spacing, twoSecondBezierControlX + 2 * spacing, twoSecondBezierControlY - 4 * spacing, twoFirstBezierAnchorX, twoFirstBezierAnchorY);  
+  noStroke();
+  fill(255);
+  beginShape();
+  vertex(bezierTwoX1, bezierTwoY1);
+  bezierVertex(bezierTwoX2, bezierTwoY2 + 2 * spacing, bezierTwoX3 + 2 * spacing, bezierTwoY3, bezierTwoX4 + 2 * spacing, bezierTwoY4);
+  bezierVertex(bezierTwoX3 - 2 * spacing, bezierTwoY3, bezierTwoX2 - 2 * spacing, bezierTwoY2  + 2 * spacing, bezierTwoX1, bezierTwoY1);
+  endShape();
 
-
+  strokeWeight(5);
+  stroke(255, 0, 255);
+  point(bezierTwoX2, bezierTwoY2);
+  stroke(0, 255, 255);
+  point(bezierTwoX3, bezierTwoY3);
 
   // angleMode(DEGREES);
   // let teardropSizeX = letterData["teardropScaleX"];
@@ -83,11 +94,11 @@ function drawLetter(letterData) {
   // bezierVertex(-30, 0, -30, 40, 0, 40);
   // endShape();
   // stroke(255);
-  // strokeWeight(3);
+  // strokeWeight(5);
   // noFill();
   // // line(-8, 38, 50, 100)
   // // beginShape();
-  // vertex(-5, 40);
+  // vertex(0, 40);
   // bezierVertex(one2X, one2Y, one3X, one3Y, one4X, one4Y);
   // bezierVertex(two2X, two2Y, two3X, two3Y, two4X, two4Y);
   // // bezierVertex(c, 40, 40, c, 40, 0);
